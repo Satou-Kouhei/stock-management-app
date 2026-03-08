10fc21c14259ffce85df33020892c802a1faebfb
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-03-08 18:33:24 +0900

    表示/登録の動作部分をプロトタイピング

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,87 @@
-import Image from "next/image";
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { useEffect, useState } from 'react';
+
+type Storages = {
+  id: number,
+  description: {
+      item: string,
+      value: number,
+    }
+}
 
 export default function Home() {
+  const [items,setItems] = useState<Storages[]>([]);
+  const [item, setItem] = useState<string>("");
+  const [value, setValue] = useState<number>(0);
+
+
+  useEffect(() => {
+    if (localStorage.length === 0) return;
+    
+    const storageItems: Storages[] = []
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (!key) continue;
+      const storages = localStorage.getItem(key);
+      if (!storages) continue;
+      
+      storageItems.push(JSON.parse(storages));
+    }
+    setItems(storageItems);
+  },[])
+
+function handleOnClick(e: React.MouseEvent<HTMLButtonElement>) {
+  const id: number = localStorage.length + 1;
+  const newData: Storages = {
+    id: id,
+    description: {
+      item: item,
+      value: value,
+    }
+  }
+
+  localStorage.setItem(newData.id.toString(), JSON.stringify(newData));
+
+  setItems(prev => [...prev, newData]);
+}
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      {items.map((items) => 
+        <Card key={items.id}>
+          <CardHeader>
+            <CardTitle>
+              ストック数
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+              <dl className='grid grid-cols-2'>
+                <dt >品目</dt>
+                <dd>{items.description.item}</dd>
+                <dt>個数</dt>
+                <dd>{items.description.value}</dd>
+              </dl>
+          </CardContent>
+        </Card>
+      )}
+
+      <p>品目を入力</p>
+      <Input type="text" placeholder='品目' value={item} onChange={prev => setItem(prev.target.value)}/>
+      <p>数量を入力</p>
+      <Input type="number" placeholder='数量' value={value} onChange={prev => setValue(Number(prev.target.value))}/>
+      <Button size="default" variant="outline" onClick={handleOnClick}>
+        追加
+      </Button>
+    </>
   );
 }
