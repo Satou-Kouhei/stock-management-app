9c603b68c944ca55135da7d0a081a6b722d04ab6
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-03-13 00:11:17 +0900

    ランディングページ用のレイアウトとページの枠組みを作成

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,16 @@
-export default function landing() {
-    return (
-        <>
-        
-        </>
-    )
+export default function landing(){
+  return(
+    <>
+      <section className='top grid grid-cols-2 content-around container mx-auto gap-6 min-h-100 bg-green-200 p-4'>
+        <img className='row-span-2 border border-black box size-100' src="/" alt="ロゴ未作成" />
+        <h1 className='content-center text-6xl'>ストック管理アプリ</h1>
+        <p>ストックを管理しましょう</p>
+      </section>
+
+      <section className='main content-around container mx-auto bg-amber-100 min-h-100'>
+        <p className='content-center text-center'>ランディングページ</p>
+        <p className='content-center text-center'>内容はこれから</p>
+      </section>
+    </>
+  );
 }
