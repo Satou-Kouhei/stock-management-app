23d6ea51d6d83065e423c31652837ed8a51a812c
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-03-13 00:09:23 +0900

    メイン部分を共通レイアウト化

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +25,22 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+        <header className='flex justify-around container mx-auto sticky top-0'>
+          <h1>ストック管理アプリ</h1>
+          <nav>
+              <ul className='flex flex-row gap-6'>
+                <li><a href="/">トップ</a></li>
+                <li><a href="/dashboard">ダッシュボード</a></li>
+                <li><a href="/stocks">一覧</a></li>
+              </ul>
+          </nav>
+        </header>
+        <main className='min-h-screen'>
+          {children}  
+        </main>
+        <footer className='text-center container mx-auto sticky bottom-0'>&copy; 2026 さとうこうへい</footer>
       </body>
     </html>
   );
