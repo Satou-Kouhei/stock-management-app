47bf78bdb3e5d57db23e5ae870dc549a8499813d
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-04-05 23:17:21 +0900

    ログイン、登録のActionにパスワードのハッシュ化を追加

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -1,7 +1,15 @@
-import { createUser, getUser } from '@/lib/db';
+import { createUser, getUserByName, userExists } from '@/lib/db';
 import { createSession, encrypt } from '@/lib/session';
+import bcrypt from 'bcryptjs';
 import { redirect } from 'next/navigation';
 
+/**
+ * 
+ * @param formData 
+ * @example
+ * <form action={loginAction}>
+ * 
+ */
 export async function loginAction(formData: FormData) {
     'use server'
 
@@ -9,11 +17,12 @@ export async function loginAction(formData: FormData) {
     const userpass = formData.get("userpass")?.toString();
     if(!username || !userpass) return;
 
-    const user = await getUser(username, userpass);
+    const user = await getUserByName(username);
     if(!user) return;
 
-    // 不正ログイン時（ハッシュ化未実装）
-    if(userpass !== user.password) return;
+    // パスワードを検証
+    const isValid = await bcrypt.compare(userpass.trim(), user.password);
+    if(!isValid) return;
 
     // JWTを作成して、Cookieに登録
     const userId: string = user.id;
@@ -25,6 +34,12 @@ export async function loginAction(formData: FormData) {
     redirect("/dashboard");
 }
 
+/**
+ * 
+ * @param formData 
+ * @example
+ * <form action={registerAction}
+ */
 export async function registerAction(formData: FormData) {
     'use server'
 
@@ -35,11 +50,12 @@ export async function registerAction(formData: FormData) {
     if(userpass !== confirm) return;
 
     // 一致するユーザー情報があれば return
-    const user = await getUser(username, userpass);
+    const user = await userExists(username);
     if(user) return;
 
     // ユーザー登録処理
-    const newUser = await createUser(username, userpass);
+    const hashedPassword = await bcrypt.hash(userpass.trim(), 12);
+    const newUser = await createUser(username, hashedPassword);
     if(!newUser) return;
 
     // JWTを作成して、Cookieに登録
