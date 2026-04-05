13fe42a5d2dff19997a40e112f0709da82ee3548
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-04-05 15:43:12 +0900

    確認用出力を削除

--- app/(protected)/action.ts
+++ app/(protected)/action.ts
@@ -5,23 +5,16 @@ import { redirect } from 'next/navigation';
 export async function addItemAction(formData: FormData) {
     "use server" 
 
-    console.log("追加開始")
     const itemName = formData.get("item-name")?.toString();
     const itemQuantity = Number(formData.get("item-quantity"));
     const itemExpires = formData.get("item-expires")?.toString()?.trim();
 
     if(!itemName || isNaN(itemQuantity)) {
-        console.log(itemName);
-        console.log(itemQuantity)
         console.log("品目と数量は必須です。");
         return;
-    } else {
-        console.log(`${itemName} : ${itemQuantity}`);
     }
 
-    console.log(itemExpires);
     const expiresAt = itemExpires ? new Date(itemExpires) : undefined;
-    console.log(expiresAt)
 
     const userId = await getUserIdBySession();
     if(userId !== "") console.log(userId);
