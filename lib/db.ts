b7a907e140dd15299e27f7e9c2e27c42034b7d4c
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-04-05 23:18:16 +0900

    関数名を変更して、コメントを追加

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,66 @@
 import { prisma } from './prisma';
 import { User, Item } from './types';
 
-export async function getUser(name: string, pass: string): Promise<User | null> {
-    return await prisma.user.findFirst({
+/**
+ * 
+ * @param name 
+ * @returns 存在する場合は true、 存在しない場合は false を返す。
+ * @example
+ * // ユーザー名から、同名ユーザーの存在確認をする。
+ * const user = await userExist(username);
+ */
+export async function userExists(name: string): Promise<boolean> {
+    const count = await prisma.user.count({
         where: {
-            name: name,
-            password: pass,
+            name: name.trim(),
         }
-    })
+    });
+    
+    return count > 0;
 }
 
+/**
+ * 
+ * @param name 
+ * @returns ユーザー名からユーザー情報を取得する。
+ */
+export async function getUserByName(name: string): Promise<{id: string, name: string, password: string} | null> {
+    return await prisma.user.findFirst({
+        where: {
+            name: name.trim(),
+        },
+        select: {
+            id: true,
+            name: true,
+            password: true,
+        }
+    });
+}
+
+/**
+ * 
+ * @param username 
+ * @param userpass ハッシュ化されたパスワード
+ * @example
+ * // ユーザーを新規登録する。
+ * const newUser = await createUser(username, userpass);
+ */
+export async function createUser(username: string, userpass: string): Promise<User> {
+    return await prisma.user.create({
+        data: {
+            name: username.trim(),
+            password: userpass
+        }
+    });
+}
+
+/**
+ * 
+ * @param userId 
+ * @returns ユーザーIDからユーザーに紐づく品目を取得する。
+ * @example
+ * const items[] | null = await getItem(userId);
+ */
 export async function getItems(userId: string): Promise<Item[] | null> {
     return await prisma.item.findMany({
         where: {
@@ -21,15 +72,16 @@ export async function getItems(userId: string): Promise<Item[] | null> {
     });
 }
 
-export async function createUser(username: string, userpass: string) {
-    return await prisma.user.create({
-        data: {
-            name: username,
-            password: userpass
-        }
-    });
-}
-
+/**
+ * 
+ * @param userId 
+ * @param itemName 
+ * @param quantity 
+ * @param expiresAt 
+ * @example
+ * // 品目を登録する
+ * await purItem(...);
+ */
 export async function putItem(userId: string, itemName: string, quantity: number, expiresAt?: Date) {
     return prisma.item.create({
         data: {
