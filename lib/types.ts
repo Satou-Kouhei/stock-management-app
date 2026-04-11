7ced64cb7334719264193e5948d98a5da1eab6d5
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-04-11 16:52:42 +0900

    Category型を定義

--- lib/types.ts
+++ lib/types.ts
@@ -3,6 +3,7 @@ import { Prisma } from '@/generated/prisma/client';
 // DB関係
 export type User = Prisma.UserGetPayload<{}>;
 export type Item = Prisma.ItemGetPayload<{}>;
+export type Category = Prisma.CategoryGetPayload<{}>;
 
 // セッション関係
 export type SessionPayload = {
