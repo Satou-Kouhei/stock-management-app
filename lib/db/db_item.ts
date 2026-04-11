365fe649f882849f0a3001bcc410660544570699
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-04-11 17:18:53 +0900

    アイテム操作処理を分離し、db.tsを削除。

--- lib/db/db_item.ts
+++ lib/db/db_item.ts
@@ -1,16 +1,11 @@
-import { prisma } from './prisma';
-import { User, Item } from './types';
-
-
-
-
-
-
+import { prisma } from '../prisma';
+import { Item } from '../types';
 
 /**
  * 
  * @param userId 
- * @returns ユーザーIDからユーザーに紐づく品目を取得する。
+ * @returns Item[]
+ * @description ユーザIDからユーザーに紐づくアイテムを全件取得する。
  * @example
  * const items[] | null = await getItem(userId);
  */
@@ -30,19 +25,20 @@ export async function getItems(userId: string): Promise<Item[] | null> {
  * @param userId 
  * @param itemName 
  * @param quantity 
- * @param expiresAt 
+ * @param categoryId
+ * @param expiresAt 省略可能
+ * @description アイテムを1件登録する。
  * @example
- * // 品目を登録する
  * await purItem(...);
  */
-export async function putItem(userId: string, itemName: string, quantity: number, expiresAt?: Date, categoryId: string) {
+export async function putItem(userId: string, itemName: string, quantity: number, categoryId: string, expiresAt?: Date) {
     return prisma.item.create({
         data: {
             userId: userId,
             name: itemName,
             quantity: quantity,
+            categoryId: categoryId,
             expiresAt: expiresAt,
-            categoryId: categoryId
 
         },
     })
@@ -52,8 +48,8 @@ export async function putItem(userId: string, itemName: string, quantity: number
  * 
  * @param itemId 
  * @param newFlg 
- * @description
- * アイテムIDと削除フラグを受け取って、論理削除の有効/無効を更新する
+ * @description アイテムIDと削除フラグを受け取って、論理削除の有効/無効を更新する
+ * @example await deleteItem(...)
  */
 export async function deleteItem(itemId: string, newFlg: boolean) {
     return prisma.item.update({
