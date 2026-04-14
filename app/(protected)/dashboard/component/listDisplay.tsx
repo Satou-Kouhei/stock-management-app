1b03e3339cce310b07ce7b1bf7ebf12571ce3d55
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-04-14 21:35:04 +0900

    ダッシュボードの一覧に分類の項目を追加

--- app/(protected)/dashboard/component/listDisplay.tsx
+++ app/(protected)/dashboard/component/listDisplay.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { getCategoryById} from '@/lib/db/db_category';
 import { getItems } from '@/lib/db/db_item';
 import { getUserIdBySession } from '@/lib/session';
 
@@ -19,29 +20,34 @@ export default async function ListDioplay() {
                     <CardContent>
                         <Card>
                             <CardContent>
-                                <ul className='grid grid-cols-3 justify-around'>
+                                <ul className='grid grid-cols-4 justify-around'>
                                     <li>品名</li>
                                     <li>数量</li>
+                                    <li>分類</li>
                                     <li>使用期限/消費期限</li>
                                 </ul>
                             </CardContent>
                         </Card>
 
                         <ul>
-                            {items.length > 0 ? items.map((item) =>
-                                item.deleteFlg ? "" :
-                                    <li key={item.id}>
-                                        <Card>
-                                            <CardContent>
-                                                <ul className='grid grid-cols-3 justify-around'>
-                                                    <li>{item.name}</li>
-                                                    <li>{item.quantity}</li>
-                                                    <li>{item.expiresAt ? item.expiresAt.toLocaleDateString("ja-jp") : ""}</li>
-                                                </ul>
-                                            </CardContent>
-                                        </Card>
-                                    </li>
-                            ) :
+                            {items.length > 0 ? items.map(async (item) => {
+                                const category = await getCategoryById(item.categoryId);
+                                return (
+                                    item.deleteFlg ? "" :
+                                        <li key={item.id}>
+                                            <Card>
+                                                <CardContent>
+                                                    <ul className='grid grid-cols-4 justify-around'>
+                                                        <li>{item.name}</li>
+                                                        <li>{item.quantity}</li>
+                                                        <li>{category}</li>
+                                                        <li>{item.expiresAt ? item.expiresAt.toLocaleDateString("ja-jp") : ""}</li>
+                                                    </ul>
+                                                </CardContent>
+                                            </Card>
+                                        </li>
+                                )
+                            }) :
                                 <Card>
                                     <CardContent>
                                         <p className='text-red-500'>まだ何も登録されていません。登録してみましょう！</p>
